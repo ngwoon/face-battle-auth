a399052a321e3d199def18b8f5aaf67a891ac0a0
ngwoon <todory2002@gmail.com>
2021-01-01 00:19:37 +0900

    Add routers

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const verRouter = require('./routes/verification');
+const regRouter = require('./routes/registration');
+const loginRouter = require('./routes/login');
+const findRouter = require('./routes/find');
+const modRouter = require('./routes/modification');
 
-var app = express();
+
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,13 +23,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
+app.use("/verification", verRouter);
+app.use("/registration", regRouter);
+app.use("/login", loginRouter);
+app.use("/find", findRouter);
+app.use("/password", modRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+}); 
 
 // error handler
 app.use(function(err, req, res, next) {
