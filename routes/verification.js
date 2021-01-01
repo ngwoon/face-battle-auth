b1acf5a4873d73cdce55300134bd0527ae0078e0
ngwoon <todory2002@gmail.com>
2021-01-01 15:14:42 +0900

    Test for develop branch commit

--- routes/verification.js
+++ routes/verification.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const router = express.Router();
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
+const router = express.Router();
 
 const HASH_STRING = "Face Battle";
 const ADMIN_MAIL_ADDRESS = "todory2002@gmail.com";
