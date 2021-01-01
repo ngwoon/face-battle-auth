493a5418bc7275dc646d9b112dc8d91241d41e54
ngwoon <todory2002@gmail.com>
2021-01-01 23:34:49 +0900

    클라이언트 ajax 코드

---
+++ public/javascripts/index.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+    const btn = $(".js-btn");
+
+    btn.on("click", (event) => {
+
+        event.preventDefault();
+
+        const email = $(".js-email").val();
+        const name = $(".js-name").val();
+        const password = $(".js-password").val();
+        const birth_date = $(".js-birth-date").val();
+        const question = $(".js-selector").val();
+        const answer = $(".js-answer").val();
+    
+        // console.log(`${email}, ${name}, ${password}, ${birth_date}, ${question}, ${answer}`);
+        
+        const url = "http://localhost:3000/registration";
+        const method = "POST";
+        const success = (data) => {
+            const res = JSON.parse(data);
+            switch(res.resultCode) {
+                case "00":
+                    alert("인증 메일이 전송되었습니다.");
+                    document.location.href = "http://localhost:3000/verification/email";
+                    break;
+                case "01":
+                    alert("이미 가입된 회원입니다.");
+                    break;
+                case "02":
+                    alert("인증 메일 전송에 실패했습니다.");
+                    break;
+                case "03":
+                    alert("서버에서 인증 코드 저장에 실패했습니다.");
+                    break;
+            }
+        };
+
+        $.ajax({
+            url,
+            method,
+            success,
+            fail: (error) => {
+                alert("ajax 에러");
+                alert(error);
+            }
+        });
+    });
+});
