3325334e1dfd68dbf3e9f3105631a0a644a934d6
ngwoon <todory2002@gmail.com>
2021-01-01 15:22:31 +0900

    users 라우터 삭제

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/users", usersRouter);
+app.use("/index", usersRouter);
 app.use("/verification", verRouter);
 app.use("/registration", regRouter);
 app.use("/login", loginRouter);
