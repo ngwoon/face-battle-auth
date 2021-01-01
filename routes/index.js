476f0aceb9a5e9f5845c58558259c3f44c830f8f
ngwoon <todory2002@gmail.com>
2021-01-02 01:09:11 +0900

    raw sql -> sequelize orm 적용

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const path = require("path");
+const db = require("../models");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const result = await db.question.findAll();
+
+  titles = []
+  for(let row of result)
+    titles.push(row.title);
+
+  res.render("index", { "titles": titles });
 });
 
 module.exports = router;
