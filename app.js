044ead33dbd00ae2570445f5c6b32c40420df73f
ngwoon <todory2002@gmail.com>
2021-01-08 23:57:59 +0900

    Modify error handling code

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const { sequelize } = require("./models");
 const jwt = require("./middlewares/jwt");
 
+const indexRouter = require("./routes/index");
 const verRouter = require('./routes/verification');
 const regRouter = require('./routes/registration');
 const loginRouter = require('./routes/login');
@@ -17,9 +18,9 @@ const app = express();
 // sequelize init
 sequelize.sync();
 
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
     routers
 */
 
-// app.use("/", indexRouter);
+app.use("/", indexRouter);
 app.use("/verification", verRouter);
 app.use("/registration", regRouter);
 app.use("/login", loginRouter);
@@ -87,15 +88,18 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
 
-    console.log(err);
+    // console.log(err);
 
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // // set locals, only providing error in development
+    // res.locals.message = err.message;
+    // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    // // render the error page
+    // res.status(err.status || 500);
+    // res.render('error');
+
+    res.status(err.resultCode || err.status).json(err);
+
 });
 
 module.exports = app;
