a0af8e9e0f172ee8d4b8d02b8a260f44562aa4b2
ngwoon <todory2002@gmail.com>
2021-01-20 03:55:40 +0900

    Remove unused import

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const crypto = require("crypto");
-const axios = require("axios");
-
-const db = require("../models");
-const jwt = require("../modules/jwt");
 const authController = require("../controllers/auth-controller");
 const router = express.Router();
 
 router.post('/normal', authController.normalLogIn);
 router.post("/oauth", authController.socialLogIn);
-
 
 module.exports = router;
